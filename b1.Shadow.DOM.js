7853bd08d4f9b1644b804f57dfdd07ea5c156f92
ibrahimcobani <ibrahim@imaconsult.com>
2017-07-16 00:31:20 +0300

    Shadow dom içinde javascript için bir capsül mekanizması yok.

--- b1.Shadow.DOM.js
+++ b1.Shadow.DOM.js
@@ -14,4 +14,31 @@ var host = document.getElementById("host");
 var root = host.createShadowRoot();
 var fasulye = document.getElementById("fasulyeden");
 //root.innerHTML = "<h2>Geçler alköl var mı?</h2>";
-root.appendChild(document.importNode(fasulye.content,true));
+root.appendChild(document.importNode(fasulye.content, true));
+
+// bu Light DOM objesini getirdi.
+console.log(document.querySelector("h1"));
+
+// Buda Shadow DOM objesini getirdi.
+console.log(root.querySelector("h1"));
+console.log(root.getElementById("msg").innerHTML);
+
+
+/**
+ * Modüle tarzı bu şekilde
+ * @type {{sayHi}}
+ */
+var module = (function () {
+  return {
+    sayHi: function () {
+      console.log('Hi');
+    },
+    sayMerhaba: function () {
+      console.log('Merhaba')
+    }
+  };
+}());
+
+
+module.sayHi();
+module.sayMerhaba();
